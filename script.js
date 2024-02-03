5a9ceffbf42606d066a078412209b0fb4c08d3d2
sreevidya-alluri <143475605+sreevidya-alluri@users.noreply.github.com>
2024-02-03 20:53:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+let container_color = document.getElementById("colorPickerContainer");
+let selectedColorHexCodeElement = document.getElementById("selectedColorHexCode");
+//let backgroundColor = document.getElementById("colorPickerContainer");
+//let changeHexcode = document.getElementById("selectedColorHexCode");
+
+function function1() {
+    container_color.style.backgroundColor = "#e0e0e0";
+    selectedColorHexCodeElement.textContent = "#e0e0e0";
+}
+
+
+function function2() {
+    container_color.style.backgroundColor = "#6fcf97";
+    selectedColorHexCodeElement.textContent = "#6fcf97";
+}
+
+function function3() {
+    container_color.style.backgroundColor = "#56ccf2";
+    selectedColorHexCodeElement.textContent = "#56ccf2"
+}
+
+function function4() {
+    container_color.style.backgroundColor = "#bb6bd9";
+    selectedColorHexCodeElement.textContent = "#bb6bd9"
+}
